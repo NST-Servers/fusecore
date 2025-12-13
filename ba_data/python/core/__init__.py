1b03445311734672661e44a20949bedc0c00a205
temp <temp3al@proton.me>
2025-12-13 13:14:21 -0600

    config port

--- ba_data/python/core/__init__.py
+++ ba_data/python/core/__init__.py
@@ -4,6 +4,9 @@ and does nothing else aside from asking for more info.
 
 from . import _bootstrap as _
 from . import discordrp
+from . import _config
 
 DiscordRP = discordrp.DiscordRPSubsystem()
 # DiscordRP.start()
+
+config = _config.ConfigSystem()
