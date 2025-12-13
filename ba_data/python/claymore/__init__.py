d16678d96fdc1823c1df4048e5cb72185673ce1c
temp <temp3al@proton.me>
2025-12-13 13:22:25 -0600

    claymore config

--- ba_data/python/claymore/__init__.py
+++ ba_data/python/claymore/__init__.py
@@ -3,9 +3,8 @@
 #         for core to load us automatically.
 import os
 
-from core import _language
+from core import _language, _config
 from .common import DATA_DIRECTORY
-
 
 # append our language dir and reload lang to apply changes
 _language.LANG_FOLDERS.append(
@@ -15,3 +14,12 @@ _language.LANG_FOLDERS.append(
     )
 )
 _language.reload_language()
+
+
+class ClaymoreConfig(_config.ConfigSystem):
+    """Custom config. instance."""
+
+    section_name = 'claymore'
+
+
+config = ClaymoreConfig()
