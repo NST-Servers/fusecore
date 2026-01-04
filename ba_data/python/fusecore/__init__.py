0eb57f23979a2d52230d09ac0bee1d47a1470a6c
temp <temp3al@proton.me>
2026-01-03 23:59:36 -0600

    even more cleanup

--- ba_data/python/fusecore/__init__.py
+++ ba_data/python/fusecore/__init__.py
@@ -6,31 +6,18 @@ import gc
 
 import bascenev1 as bs
 
-from . import (
-    common,
-    _preload as pre,
-    _bootstrap as boots,
-    _modloader,
-    serverqueue,
-    _config,
-    discordrpc,
-    server,
+from . import common
+
+from ._bootstrap import (
+    _PreloadManager,
+    ServerManager,
+    DiscordRPC,
+    ModLoader,
+    ServerQueue,
+    config,
 )
 
 # hot code above; ballistica's py gc funnel doesn't like that.
 # specifically: some of our own packages and libraries get
 # some references going, we gotta clean those to prevent errors.
 gc.collect()
-
-common.init_dirs()
-
-_PreloadManager = pre.AssetLoadManager()
-ServerManager = server.FCServerManager()
-DiscordRPC = bs.app.register_subsystem(
-    discordrpc.DiscordRichPresenceSubsystem()
-)
-ModLoader = bs.app.register_subsystem(_modloader.ModLoaderSubsystem())
-ServerQueue = bs.app.register_subsystem(serverqueue.ServerQueueSubsystem())
-
-
-config = _config.ConfigSystem()
