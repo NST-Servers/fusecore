1665b1eeb9f7faa739b6b1edc85fc8bb4f83744b
temp <temp3al@proton.me>
2026-01-15 23:15:31 -0600

    better demoviewer

--- ba_data/python/fusecore/demoviewer.py
+++ ba_data/python/fusecore/demoviewer.py
@@ -1,5 +1,8 @@
 """Module for loading and playing replays from set directories."""
 
+from pathlib import Path
+from typing import Union
+
 import os
 import random
 import logging
@@ -7,54 +10,50 @@ import logging
 import bauiv1 as bui
 import bascenev1 as bs
 
-from .common import DATA_DIRECTORY
 
+def get_replays_from_dir(dir_path: Union[Path, str]) -> list[Path]:
+    """Get all replay files from a specific directory."""
+    # transform strings into proper paths
+    if isinstance(dir_path, str):
+        dir_path = Path(dir_path)
 
-REPLAY_FOLDERS: list[str] = [os.path.join(DATA_DIRECTORY, "replays")]
+    if not dir_path.exists() or not dir_path.is_dir():
+        raise FileNotFoundError(f"invalid path: '{dir_path}'")
 
-
-def _get_replays_dir_from_path(path: str) -> list[str]:
-    output: list[str] = []
-
-    if not (os.path.exists(path) and os.path.isdir(path)):
-        raise FileNotFoundError(f"invalid path: '{path}'")
-
-    for filename in os.listdir(path):
-        if os.path.splitext(filename)[1].lower() == ".brp":
-            output.append(os.path.join(path, filename))
-
+    output: list[Path] = []
+    # look for any files with a '.brp' extension and send
+    # it to our output list.
+    for file_path in [Path(f) for f in os.scandir(dir_path)]:
+        if file_path.suffix.lower() == ".brp":
+            output.append(file_path)
+    # export it once we're done.
     return output
 
 
-def get_user_replays() -> list[str]:
+def get_user_replays() -> list[Path]:
     """Get all saved replays from the user's replay folder."""
-    return _get_replays_dir_from_path(bui.get_replays_dir())
+    return get_replays_from_dir(bui.get_replays_dir())
 
 
-def get_demo_replays() -> list[str]:
-    """Get all replays from the 'REPLAY_FOLDERS' list."""
-    replays: list[str] = []
-    for folder_path in REPLAY_FOLDERS:
-        _get_replays_dir_from_path(folder_path)
-
-    return replays
-
-
-def launch_replay_from_list(replay_path_list: list[str]) -> None:
+def launch_replay(replay_path: Union[Path, list[Path]]) -> None:
     """Read a random replay file from the provided list
     and launch a replay activity using it.
     """
-    if len(replay_path_list) < 1:
-        return
-
-    path = random.choice(replay_path_list)
+    path: Path
+    if isinstance(replay_path, Path):
+        path = replay_path
+    else:
+        if len(replay_path) < 1:
+            raise ValueError("empty path list provided.")
+        # choose a random replay
+        path = random.choice(replay_path)
 
     def do_it() -> None:  # efro code :]
         try:
             # Reset to normal speed.
             bs.set_replay_speed_exponent(0)
             bui.fade_screen(True)
-            bs.new_replay_session(path)
+            bs.new_replay_session(str(path))
         except RuntimeError:
             logging.exception("Error running replay session.")
 
